68438407935b3c6212f24973d8150b192e7bccf4
Mateusz Bednarski <mateusz.bednarski@windowslive.com>
2020-03-15 12:50:51 +0100

    handling args

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-int main(){
-	puts("Hello from git-managed world!");
-	return 0;
+int main(int argc, char** argv){
+    puts("Hello from git-managed world!");
+    puts("I can handle args");
+    return 0;
 }
+
